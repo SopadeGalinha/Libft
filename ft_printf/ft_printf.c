bb23bf94023607b3e9255dd645f04fdeb2f4726b
Pw <Uhpere@gmail.com>
2022-03-05 01:32:01 +0000

    Progress..

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,56 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/libft.h"
 #include <stdio.h>
 #include <stdarg.h>
 
-int ft_printarg(const char *arg, int i, va_list lst)
+int ft_checkflag(char f)
 {
-    if (arg[i + 1] == '%')
-        return (ft_putchar(arg[i + 1]));
-    else if (arg[i + 1] == 'c')
-        return (ft_putchar(va_arg(ap, char)));
-    else if (arg[i + 1] == 's')
-        return (ft_putstr(va_arg(ap, char *)));
-    else if (arg[i + 1] == 'p')
-        return ();
-    else if (arg[i + 1] == 'i' || arg[i + 1] == 'd')
-        return (ft_putnbr(va_arg(ap, int)));
-/*
-    else if (arg[i + 1] == 'u')
-        return ();
-    else if (arg[i + 1] == 'x')
-        return ();
-    else if (arg[i + 1] == 'X')
-        return ();
-*/
+    if (f == 'c' || f == 's' || f == '%'
+        || f == 'd' || f == 'i' || f == 'u'
+        || f == 'p' || f == 'x' || f == 'X')
+        return (1);
+    return (0);
 }
 
-int ft_getflag(const char *fmt, int i, va_list lst)
+int ft_conversions(char fmt, va_list args)
 {
-
+    int c;
+    
+    c = 0;
+    if (fmt == 'c' || fmt == '%')
+        c += write(1, &fmt, 1);
+    else if (fmt == 'd' || fmt == 'i')
+        //c += ft_putnbr(va_arg(args, int));
+    return (c);
 }
+
 int ft_printf(const char *fmt, ...)
 {
-    va_list lst;
-    int     i;
-    int     res;
+    int i;
+    int counter;
+    va_list args;
 
-    i = 0;
-    res = 0;
-    while (fmt[i])
+    i = -1;
+    va_start(args, fmt);
+    while (fmt[++i])
     {
-        if (ft_strchr(fmt[i] == '%'))
-        {
-            i++;
-            if (fmt[i] && ft_strchr("cspdiuxX", fmt[i]))
-            {
-                res += (((int (*)())ft_getf(str[i++]))(va_arg(l, void *), 1));
-                res += (ft_getflag(fmt, i, lst));
-            }
-        }
-        write(1, &fmt[i++] 1);
-        res++;
+        if (!fmt)
+            return (counter);
+        if (fmt[i] == '%')
+            if (ft_checkflag(fmt[++i]))
+                counter += ft_conversions(fmt[i], args);
+            else
+                continue ;
+        else
+            counter += write(1, &fmt[i], 1);
     }
+    va_end(args);
+    return (counter);
+}
+
+int main(void)
+{
+    
+    ft_printf("abc %c\n", 'a');
 }
